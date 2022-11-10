fd762cc9d41449b9d4cb5674c73b76ca82a6e67e
r1d3rzz <myintthwaykhine127@gmail.com>
2022-11-10 15:08:45 +0630

    super global server variable

--- index.php
+++ index.php
@@ -1,35 +1,38 @@
 <?php
 
-if (isset($_POST['submit'])) {
+echo "<pre>";
+print_r($_SERVER);
 
-    $p_1 = 0;
-    $p_2 = 0;
-    $d = 0;
 
-    for ($i = 0; $i < 100; $i++) {
-        $player_1 = rand(0, 9);
-        $player_2 = rand(0, 9);
+// Super Global Variable $_Server //
 
-        if ($player_1 > $player_2) {
-            $p_1++;
-        } else if ($player_1 < $player_2) {
-            $p_2++;
-        } else if ($player_1 == $player_2) {
-            $d++;
-        }
-    }
-    echo "Player 1 is Win $p_1 times <br/>";
-    echo "Player 2 is Win $p_2 times <br/>";
-    echo "Draw $d times <br/>";
-    if ($p_1 > $p_2) {
-        echo "So Player 1 is Winner";
-    } else {
-        echo "So Player 2 is Winner";
-    }
-}
-
-?>
-
-<form action="" method="POST">
-    <input type="submit" value="start" name="submit">
-</form>
+    // [DOCUMENT_ROOT] => C:\code\php
+    // [REMOTE_ADDR] => ::1
+    // [REMOTE_PORT] => 58409
+    // [SERVER_SOFTWARE] => PHP 8.1.6 Development Server
+    // [SERVER_PROTOCOL] => HTTP/1.1
+    // [SERVER_NAME] => localhost
+    // [SERVER_PORT] => 8000
+    // [REQUEST_URI] => /?name=rider
+    // [REQUEST_METHOD] => GET
+    // [SCRIPT_NAME] => /index.php
+    // [SCRIPT_FILENAME] => C:\code\php\index.php
+    // [PHP_SELF] => /index.php
+    // [QUERY_STRING] => name=rider
+    // [HTTP_HOST] => localhost:8000
+    // [HTTP_CONNECTION] => keep-alive
+    // [HTTP_CACHE_CONTROL] => max-age=0
+    // [HTTP_SEC_CH_UA] => "Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"
+    // [HTTP_SEC_CH_UA_MOBILE] => ?0
+    // [HTTP_SEC_CH_UA_PLATFORM] => "Windows"
+    // [HTTP_UPGRADE_INSECURE_REQUESTS] => 1
+    // [HTTP_USER_AGENT] => Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36
+    // [HTTP_ACCEPT] => text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9
+    // [HTTP_SEC_FETCH_SITE] => none
+    // [HTTP_SEC_FETCH_MODE] => navigate
+    // [HTTP_SEC_FETCH_USER] => ?1
+    // [HTTP_SEC_FETCH_DEST] => document
+    // [HTTP_ACCEPT_ENCODING] => gzip, deflate, br
+    // [HTTP_ACCEPT_LANGUAGE] => en-GB,en;q=0.9,my-MM;q=0.8,my;q=0.7,en-US;q=0.6
+    // [REQUEST_TIME_FLOAT] => 1668069429.7313
+    // [REQUEST_TIME] => 1668069429
