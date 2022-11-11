2e0235626f5c0684d723f6f710d7b8bb300349d2
r1d3rzz <myintthwaykhine127@gmail.com>
2022-11-11 14:12:24 +0630

    error handling

--- index.php
+++ index.php
@@ -1,75 +1,31 @@
 <?php
 
-error_reporting(1);
+$file = "text.txt"; //test with change file variable value eg.tests.txt so that u can see the error Handling Effect
 
-session_start();
+// Three Types of Handling => die , exit, try & catch
 
-$name = $_POST['name'];
-$pass = $_POST['pass'];
-$login = $_POST['login'];
-$checked = $_POST['checked'];
+//using exit();
+// if (!file_exists($file)) {
+//     exit('File not Exits');
+// }
 
 
-if (isset($login)) {
-    if ($name == 'Rider' && $pass == "rider") {
-        $_SESSION['name'] = $name;
-        if ($checked) {
-            setcookie('name', $name, time() + 20, '/');
-            setcookie('pass', $pass, time() + 20, '/');
-        }
-        header('location:welcome.php');
+//using die();
+// if (!file_exists($file)) {
+//     die('File not Exits');
+// }
+
+
+//using try and catch 
+try {
+    if (file_exists($file)) {
+        $handler = fopen($file, 'r');
+        $fileText = fread($handler, filesize($file));
+        fclose($handler);
+        echo $fileText;
     } else {
-        echo "<script>alert('Wrong Password or Username')</script>";
-        echo "<script>location='index.php'</script>";
+        throw new Exception('File not Found!');
     }
+} catch (Exception $e) {
+    echo $e->getMessage();
 }
-
-?>
-
-<div class="inputValue">
-    <div>Username => Rider</div>
-    <div>Password => rider</div>
-    <div>Cookie Expire Time => 20s</div>
-</div>
-
-<form action="" method="POST">
-    <table>
-        <tr>
-            <td>Username</td>
-            <td><input type="text" name="name" id="" value="<?= $_COOKIE['name']; ?>"></td>
-        </tr>
-        <tr>
-            <td>Password</td>
-            <td><input type="password" name="pass" id="" value="<?= $_COOKIE['pass']; ?>"></td>
-        </tr>
-        <tr>
-            <td colspan="2" align="end"><input type="submit" value="Login" name="login"></td>
-        </tr>
-        <tr>
-            <td colspan="2" align="center">
-                <div class="checkContainer">
-                    <div><input type="checkbox" name="checked" id="check"></div>
-                    <div><label for="check">remember me?</label></div>
-                </div>
-            </td>
-        </tr>
-    </table>
-</form>
-
-<style>
-    .checkContainer {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-
-    .inputValue {
-        border: 1px solid cyan;
-        width: 200px;
-        text-align: start;
-        padding: 10px;
-        margin-bottom: 20px;
-        border-radius: 5px;
-        box-shadow: 6px 6px 10px black;
-    }
-</style>
