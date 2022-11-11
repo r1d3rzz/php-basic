b18e077e9f839335b5f3f5200c56aa0bf22823c2
r1d3rzz <myintthwaykhine127@gmail.com>
2022-11-11 19:25:30 +0630

    connect with mysqli db

--- index.php
+++ index.php
@@ -1,44 +1,22 @@
 <?php
 
-$name = "Rider";
-$age = 22;
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'test');
 
-// ------------Diff between printf and fprintf------------
-
-// printf("Hello My name is %s and age is %u years old.", $name, $age);
-
-// $handler = fopen('text.txt', 'w');
-// fprintf($handler, 'Hello my name is %s and age is %u years old.', $name, $age);
-// fclose($handler);
-
-//Create XML File
-
-$file = "text.xml";
-
-$xmlText = [
-    'name' => 'rider',
-    'age' => 22,
-    'gender' => 'male',
-    'job' => 'studends',
-    'code' => 'php'
-];
-
-$str = "";
-
-$str .= "<program>";
-foreach ($xmlText as $key => $val) {
-    $str .= "<" . $key . ">";
-    $str .=   $val;
-    $str .= "</" . $key . ">";
-};
-$str .= "</program>";
-
-$handler = fopen($file, 'w');
-fprintf($handler, '%s', $str);
-fclose($handler);
-
-$xml = simplexml_load_file('text.xml');
-
-foreach ($xml as $key => $val) {
-    echo $key . " = " . $val . "<br/>";
+function dd($data) //error or bug checker function ;)
+{
+    echo "<pre>";
+    die(print_r($data, true));
 }
+
+function dbConnect()
+{
+    $db = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    if (!mysqli_errno($db) > 0) {
+        return '<h3>Connected Successfully</h3>';
+    }
+}
+
+dd(dbConnect());
