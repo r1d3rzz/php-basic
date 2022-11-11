f227ad086b51a0f1ec9ff3617065514f68b13afc
r1d3rzz <myintthwaykhine127@gmail.com>
2022-11-11 13:43:38 +0630

    create login auth system example with session and cookie

--- index.php
+++ index.php
@@ -1,19 +1,75 @@
 <?php
 
-$password = "password";
+error_reporting(1);
 
-echo "Original Value = " . $password . "<br/>";
-echo "<br/>";
-echo "md5 false = " . md5($password, false) . "<br/>";
-echo "<br/>";
-echo "md5 true = " . md5($password, true) . "<br/>";
-echo "<br/>";
-echo "sha1 false = " . sha1($password, false) . "<br/>";
-echo "<br/>";
-echo "sha1 true = " . sha1($password, true) . "<br/>";
-echo "<br/>";
-echo "crypt = " . crypt($password, $password) . "<br/>";
-echo "<br/>";
-echo "all combine = " . crypt(sha1(md5($password, true)), $password) . "<br/>";
-echo "<br/>";
-echo "all combine extra* = " . crypt(sha1(md5($password, true), true), sha1(md5($password))) . "<br/>";
+session_start();
+
+$name = $_POST['name'];
+$pass = $_POST['pass'];
+$login = $_POST['login'];
+$checked = $_POST['checked'];
+
+
+if (isset($login)) {
+    if ($name == 'Rider' && $pass == "rider") {
+        $_SESSION['name'] = $name;
+        if ($checked) {
+            setcookie('name', $name, time() + 20, '/');
+            setcookie('pass', $pass, time() + 20, '/');
+        }
+        header('location:welcome.php');
+    } else {
+        echo "<script>alert('Wrong Password or Username')</script>";
+        echo "<script>location='index.php'</script>";
+    }
+}
+
+?>
+
+<div class="inputValue">
+    <div>Username => Rider</div>
+    <div>Password => rider</div>
+    <div>Cookie Expire Time => 20s</div>
+</div>
+
+<form action="" method="POST">
+    <table>
+        <tr>
+            <td>Username</td>
+            <td><input type="text" name="name" id="" value="<?= $_COOKIE['name']; ?>"></td>
+        </tr>
+        <tr>
+            <td>Password</td>
+            <td><input type="password" name="pass" id="" value="<?= $_COOKIE['pass']; ?>"></td>
+        </tr>
+        <tr>
+            <td colspan="2" align="end"><input type="submit" value="Login" name="login"></td>
+        </tr>
+        <tr>
+            <td colspan="2" align="center">
+                <div class="checkContainer">
+                    <div><input type="checkbox" name="checked" id="check"></div>
+                    <div><label for="check">remember me?</label></div>
+                </div>
+            </td>
+        </tr>
+    </table>
+</form>
+
+<style>
+    .checkContainer {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .inputValue {
+        border: 1px solid cyan;
+        width: 200px;
+        text-align: start;
+        padding: 10px;
+        margin-bottom: 20px;
+        border-radius: 5px;
+        box-shadow: 6px 6px 10px black;
+    }
+</style>
