9b388079dda474b762dbd29867559c4c9b15a047
r1d3rzz <myintthwaykhine127@gmail.com>
2022-11-12 09:44:52 +0630

    fetch data from mysql

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'test');
 
-function dd($data) //error or bug checker function ;)
+function dd($data)
 {
     echo "<pre>";
     die(print_r($data, true));
@@ -15,8 +15,24 @@ function dbConnect()
 {
     $db = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
     if (!mysqli_errno($db) > 0) {
-        return '<h3>Connected Successfully</h3>';
+        return $db;
+    };
+}
+
+function excuteQuery($query)
+{
+    $result = mysqli_query(dbConnect(), $query);
+    if (mysqli_num_rows($result) > 0) {
+        echo "<h3>Total Users (" . mysqli_num_rows($result) . ")</h3>";
+        foreach ($result as $item) {
+            echo 'Name = ' . $item['name'] . "<br/>";
+            echo 'Email = ' . $item['email'] . "<br/>";
+            echo 'Password = ' . $item['password'] . "<br/>";
+            echo "<hr/>";
+        }
     }
 }
 
-dd(dbConnect());
+$query = 'SELECT * FROM users';
+
+excuteQuery($query);
