9ceb12864e1fa741abada73b8e4f400f68780c21
r1d3rzz <myintthwaykhine127@gmail.com>
2022-11-12 10:18:00 +0630

    filter user data with id

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+error_reporting(1);
+
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
@@ -25,6 +27,7 @@ function excuteQuery($query)
     if (mysqli_num_rows($result) > 0) {
         echo "<h3>Total Users (" . mysqli_num_rows($result) . ")</h3>";
         foreach ($result as $item) {
+            echo 'Id = ' . $item['id'] . "<br/>";
             echo 'Name = ' . $item['name'] . "<br/>";
             echo 'Email = ' . $item['email'] . "<br/>";
             echo 'Password = ' . $item['password'] . "<br/>";
@@ -33,6 +36,51 @@ function excuteQuery($query)
     }
 }
 
-$query = 'SELECT * FROM users';
+function singleData($id)
+{
+    $query = "SELECT * FROM users WHERE id = $id";
+    $result = mysqli_query(dbConnect(), $query);
+    if (!mysqli_num_rows($result) > 0) {
+        echo "<h3>User Id = $id not Found</h3>";
+    }
+    if (mysqli_num_rows($result) > 0) {
+        echo "<h3>Total Users (" . mysqli_num_rows($result) . ")</h3>";
+        foreach ($result as $item) {
+            echo 'Id = ' . $item['id'] . "<br/>";
+            echo 'Name = ' . $item['name'] . "<br/>";
+            echo 'Email = ' . $item['email'] . "<br/>";
+            echo 'Password = ' . $item['password'] . "<br/>";
+            echo "<hr/>";
+        }
+    }
+}
 
-excuteQuery($query);
+
+
+$id = $_POST['id'];
+$search = $_POST['search'];
+if (isset($search)) {
+    if ($id == '') {
+        $query = 'SELECT * FROM users';
+        excuteQuery($query);
+    } else {
+        singleData($id);
+    }
+}
+
+
+?>
+
+<form action="" method="POST">
+    <table border="1">
+        <tr>
+            <td>Enter User ID</td>
+            <td><input type="number" name="id"></td>
+        </tr>
+        <tr>
+            <td colspan="2" align="end">
+                <input type="submit" value="Search" name="search">
+            </td>
+        </tr>
+    </table>
+</form>
